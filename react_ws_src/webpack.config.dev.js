4a35ff0bc678cd50dbe693909b7ca36f5ce75cea
ishak ertem <ertemishakk@gmail.com>
2025-12-03 12:47:52 +1100

    get it to run

--- react_ws_src/webpack.config.dev.js
+++ react_ws_src/webpack.config.dev.js
@@ -1,49 +1,62 @@
-var path = require('path')
-var webpack = require('webpack')
+var path = require("path");
+var webpack = require("webpack");
 
 module.exports = {
-	devtool: 'cheap-module-eval-source-map',
-	// context: path.join(__dirname, 'static'),
-	entry: [
-		'eventsource-polyfill', // necessary for hot reloading with IE
-		'webpack-hot-middleware/client',
-		'./src/app'
-	],
-	output: {
-		path: path.join(__dirname, 'dist'),
-		filename: 'bundle.js',
-		// publicPath: path.join(__dirname, 'static'),
-		// publicPath: __dirname + '/static/'
-		publicPath: '/'
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
-		new webpack.NoErrorsPlugin()
-	],
-	module: {
-		loaders: [
-			{
-				test: /\.(ico|css|gif|png|html|jpg|xml|svg)$/,
-					// loader: 'url?limit=10000',
-				loader: 'file?name=[path][name].[ext]&context=./static',
-			},
-			{
-				test: /\.jsx?/,
-				loaders: ['babel'],
-				include: path.join(__dirname, 'src')
-			},
-			{
-				test: /\.scss$/,
-				loaders: ['style', 'css', 'sass']
-			},
-			{
-				test: /\.css$/,
-				loaders: ['style', 'css']
-			},
-			{
-				test: /(flickity|fizzy-ui-utils|get-size|unipointer|imagesloaded)/,
-				loader: 'imports?define=>false&this=>window'
-			},
-		]
-	},
-}
+  devtool: "cheap-module-eval-source-map",
+  // context: path.join(__dirname, 'static'),
+  entry: [
+    "eventsource-polyfill", // necessary for hot reloading with IE
+    "webpack-hot-middleware/client",
+    "./src/app",
+  ],
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+    // publicPath: path.join(__dirname, 'static'),
+    // publicPath: __dirname + '/static/'
+    publicPath: "/",
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.(ico|css|gif|png|html|jpg|xml|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[path][name].[ext]",
+              context: path.resolve(__dirname, "static"),
+              esModule: false,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.jsx?$/,
+        include: path.resolve(__dirname, "src"),
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /(flickity|fizzy-ui-utils|get-size|unipointer|imagesloaded)/,
+        use: {
+          loader: "imports-loader",
+          options: {
+            additionalCode: "var define = false;",
+            wrapper: "window",
+          },
+        },
+      },
+    ],
+  },
+};
